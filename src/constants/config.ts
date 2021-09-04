dc60007c5b5390d66100fc60f18d1f195d73978a
chadmuro <chadmuro@gmail.com>
2021-09-04 10:07:26 +0900

    update constant routes again to remove /api

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,13 +1,13 @@
 const production = {
   url: {
-    API_URL: 'https://chadmuro-japanese-api.herokuapp.com/api',
+    API_URL: 'https://chadmuro-japanese-api.herokuapp.com',
     FRONT_URL: 'https://japanese-for-developers.netlify.app',
   },
 };
 
 const development = {
   url: {
-    API_URL: 'http://localhost:5000/api',
+    API_URL: 'http://localhost:5000',
     FRONT_URL: 'http://localhost:3000',
   },
 };
